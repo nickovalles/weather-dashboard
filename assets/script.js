c4e3d6c82d2d2dd0efaaf04bf2dfeb97b90b24ac
Nilda <nick.ovalles@gmail.com>
2020-12-13 10:45:26 -0700

    <APIs and JS file constants>

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Create constants
+const searchButtonEl = document.getElementById("search-button");
+const cityNameEl = document.getElementById("city-name");
+const cityEntryEl = document.getElementById("city-entry");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+console.log(searchHistory);
